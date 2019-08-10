4f5d78739200b50a9eff7ee7b4a0f1b7ecc9192f
James Porter <james@amimetic.co.uk>
2019-08-10 21:55:49 +0100

    scaler util and tests

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -5,6 +5,22 @@ export const clamp = (
   return Math.min(to, Math.max(from, n))
 }
 
+export const scaler = ({
+  minDomain,
+  maxDomain,
+  minRange,
+  maxRange,
+}: {
+  minDomain: number
+  maxDomain: number
+  minRange: number
+  maxRange: number
+}): ((n: number) => number) => {
+  const rangeS = maxRange - minRange
+  const domainS = maxDomain - minDomain
+  return n => minRange + (rangeS * (n - minDomain)) / domainS
+}
+
 /**
  * @param height The height of the (vertical parts of) isometric grid cells
  * @returns A function mapping from [x,y,z] to [x,y].
