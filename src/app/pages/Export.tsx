18f919adced0a550b224811f77a6fe3776b3b96f
James Porter <james@amimetic.co.uk>
2019-08-10 22:16:55 +0100

    do a save button

--- src/app/pages/Export.tsx
+++ src/app/pages/Export.tsx
@@ -12,11 +12,21 @@ import { getNumber, getSketchIdx } from "../util"
 import { SEED_KEY, TIME_KEY } from "./ViewSingle"
 import Header from "../components/Header"
 
+function downloadURI(uri, name) {
+  let link = document.createElement("a")
+  link.download = name
+  link.href = uri
+  document.body.appendChild(link)
+  link.click()
+  document.body.removeChild(link)
+}
+
 export function Export() {
   const seed = getNumber(SEED_KEY) || 1
   const time = getNumber(TIME_KEY) || 0
   const [aspectRatio, setAspectRatio] = useState(defaultAspectRatio)
   const [size, setSize] = useState(defaultSize)
+  const [data, setData] = useState<string | null>(null)
   const previewRef = useRef<HTMLCanvasElement | null>(null)
   const sketchNo = getSketchIdx()
   const sketch = sketches[sketchNo || 0]
@@ -40,6 +50,7 @@ export function Export() {
         time
       )
       sketch.sketch(pts)
+      setData(previewRef.current && previewRef.current.toDataURL())
     }
   }
 
@@ -62,12 +73,29 @@ export function Export() {
 
         <button
           onClick={generate}
-          className="bg-teal-500 hover:bg-teal-600 focus:outline-none focus:shadow-outline px-2 py-3 rounded mt-4"
+          className="bg-teal-500 hover:bg-teal-600 focus:outline-none focus:shadow-outline px-2 py-4 rounded mt-4"
         >
-          Generate {w}x{h}
+          Generate {w}x{h} (Size: {Math.floor((w * h) / 1000000)} MP)
         </button>
 
-        <p>Size: {Math.floor((w * h) / 1000000)} MP</p>
+        {data && (
+          <>
+            <a
+              onClick={() => {
+                downloadURI(data, "sol.png")
+              }}
+              className="ml-2 bg-teal-500 hover:bg-teal-600 focus:outline-none focus:shadow-outline px-4 py-4 rounded mt-4 inline-block"
+            >
+              Save
+            </a>
+            <p>
+              Browser support for saving varies e.g. Chrome rejects large image
+              sizes (but will allow right click/save as), Safari is slower to
+              render (and fails at larger sizes) but will allow saving of larger
+              images.
+            </p>
+          </>
+        )}
 
         <canvas
           width={size}
