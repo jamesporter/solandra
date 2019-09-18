da1896bec99ccf50ff8f3663a58e08d10628e82e
James Porter <james@amimetic.co.uk>
2019-09-18 22:08:36 +0100

    feat: add shadow, dashes, another line thing
    to main api.
    
    add 3 examples
    bump version

--- src/examples/advanced-apis.ts
+++ src/examples/advanced-apis.ts
@@ -1,6 +1,6 @@
 import SCanvas from "../lib/sCanvas"
 import { Path, CompoundPath, Star, RegularPolygon, Circle } from "../lib/paths"
-import { v } from "../lib"
+import { v, Rect } from "../lib"
 
 const compoundPath = (p: SCanvas) => {
   p.background(45, 80, 75)
@@ -87,11 +87,76 @@ const orderedTiles = (p: SCanvas) => {
   )
 }
 
+const shadows = (p: SCanvas) => {
+  p.background(10, 30, 95)
+  p.forTiling(
+    { n: 6, type: "square", order: "rowFirst", margin: 0.05 },
+    (pt, [dX], _c, t) => {
+      const i = t % 6
+      const j = Math.floor(t / 6)
+
+      p.setFillColour(t, 90, 40, 0.75)
+      p.shadow = { size: t * 0.001, dX: (i - 2.5) * 0.01, dY: j * 0.01 }
+      p.fill(
+        new Rect({
+          at: v.add([dX / 6, dX / 6], pt),
+          w: (dX * 2) / 3,
+          h: (dX * 2) / 3,
+        })
+      )
+    }
+  )
+}
+
+const dashes = (p: SCanvas) => {
+  p.background(0, 0, 5)
+  p.forTiling({ n: 5, margin: 0.1, type: "square" }, (_pt, [dX], at, i) => {
+    p.lineWidth = 0.005
+    p.dash = { offset: p.t / 20, pattern: [0.001 * (5 + i), 0.002 * (5 + i)] }
+    p.setStrokeColour(45 + i * 10, 100, 70, 0.9)
+    p.draw(new RegularPolygon({ at, n: 6, r: dX / 3 }))
+  })
+}
+
+const glow = (p: SCanvas) => {
+  p.background(0, 0, 5)
+  p.forTiling(
+    { n: 5, type: "square", order: "rowFirst", margin: 0.05 },
+    (pt, [dX], c, t) => {
+      const i = t % 5
+      const j = Math.floor(t / 5)
+
+      p.setFillColour(t + 150, 90, 40, 0.75)
+      p.shadow = {
+        dX: 0,
+        dY: 0,
+        size: t * 0.0025 * p.oscillate({ from: 0.5, to: 1.5 }),
+        colour: {
+          h: t + 150,
+          s: 50,
+          l: 100,
+          a: 0.9,
+        },
+      }
+      p.fill(
+        new RegularPolygon({
+          at: c,
+          r: dX / 3,
+          n: p.sample([3, 5, 7]),
+        })
+      )
+    }
+  )
+}
+
 const sketches: { name: string; sketch: (p: SCanvas) => void }[] = [
   { sketch: compoundPath, name: "Compound Path" },
   { sketch: compoundPath2, name: "Compound Path 2" },
   { sketch: transformingPaths, name: "Transforming Paths" },
   { sketch: orderedTiles, name: "Ordering of Tiles" },
+  { sketch: dashes, name: "Dashes" },
+  { sketch: shadows, name: "Shadows" },
+  { sketch: glow, name: "Glow" },
 ]
 
 export default sketches
