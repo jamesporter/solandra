5a3e023596a18c5aa205d1606bae5aeba7d3db1e
James Porter <james@amimetic.co.uk>
2026-04-10 22:48:08 +0100

    basic updates
    seems like need --webpack for mdx?

--- tailwind.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  content: ["./pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("@tailwindcss/container-queries")],
-}
